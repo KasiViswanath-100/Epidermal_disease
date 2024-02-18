247c61b69cc6e3f2101bdaeccbd984572ff331e1
Kasi Viswanath Nilla <kvisu2003@gmail.com>
2024-02-18 20:50:11 +0530

    Update skin_deploy.py

--- skin_deploy.py
+++ skin_deploy.py
@@ -1,68 +1,41 @@
 import streamlit as st
-import tensorflow as tf
 import numpy as np
 from PIL import Image
-import keras
+import tensorflow as tf
+from keras.applications.vgg16 import preprocess_input
+from keras.models import load_model
 
-@st.cache(allow_output_mutation=True)
-# Load your pre-trained model
+@st.cache_resource
 def load_model():
 	model = tf.keras.models.load_model('Epidermal_Disease.h5')
 	return model
 with st.spinner('Loading Model into Memory....'):
     model = load_model()
-# model = keras.models.load_model('Epidermal_Disease.h5')
-
-# Define the skin disease categories
 disease_categories = ['Acne and Rosacea Photos', 'Eczema Photos', 'Exanthems and Drug Eruptions', \
                       'Light Diseases and Disorders of Pigmentation', 'Lupus and other Connective Tissue diseases', \
                       'Poison Ivy Photos and other Contact Dermatitis', 'Psoriasis pictures Lichen Planus and related diseases', \
                       'Scabies Lyme Disease and other Infestations and Bites', \
                       'Systemic Disease', 'Warts Molluscum and other Viral Infections']
 
-# st.title("Skin Disease Classification")
-
-# Upload an image
-# image = st.file_uploader("Upload an image", type=["jpg", "png"])
-# model = tf.keras.models.load_model('Epidermal_Disease.h5')
-def predict_class(image, model):
-
-	image = tf.cast(image, tf.float32)
-	image = tf.image.resize(image, [224,224])
-
-	image = np.expand_dims(image, axis = 1 )
-
-	prediction = model.predict(image)
-
-	return prediction
-
-# pred_probs = model.predict(uploaded_image)
-# pred_probs[0]
-# pred_classes = pred_probs.argmax(axis =1)
-# print(pred_classes[0])
-# print(disease_categories[pred_classes[0]])
-
-
+def preprocess_image(image):
+    img = image.resize((224, 224))
+    img = np.array(img)
+    # img = np.expand_dims(img, axis=0)
+    return img
 
 st.image('logo_skin.jpg', width=100 )
 st.title('Epidermal Disease Classifier')
+st.write('Upload an image for classification')
 
-file = st.file_uploader("Upload an image of a disease", type=["jpg", "png"])
+uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'png'])
 
-if file is None:
-    st.text('Waiting for upload...')
-    
-else:
-    slot = st.empty()
-    slot.text('Running the Inference...')
-    test_image = Image.open(file)
-    st.image(test_image, caption='Input Image', width= 500)
-    pred = predict_class(test_image, model)
-    # score = tf.nn.softmax(pred[0])
-    # pred_probs = model.predict(test_image)
-    # pred_probs[0]
-    pred_classes = pred.argmax(axis=0)
-    # print(pred_classes[0])
-    # print(disease_categories[pred_classes[0]])
-    output = "This image most likely belongs to {} .".format(disease_categories[pred_classes[0]])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)    
+    processed_image = preprocess_image(image)    
+    prediction = model.predict(processed_image)
+    predicted_class = np.argmax(prediction,axis=0)
+    print("Predicted class:", predicted_class)
+    output = "This image most likely belongs to {} .".format(disease_categories[predicted_class[0]])
+    # st.write('Prediction:')
     st.success(output)
